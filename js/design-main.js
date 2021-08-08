dfe778965b10c0501915b5cd02698f38984f7580
Ethan Georlette <ethan.georlette1@gmail.com>
2021-08-08 13:22:59 +0300

    adjusted css

--- js/design-main.js
+++ js/design-main.js
@@ -1,11 +1,11 @@
 'use strict'
 function toggleSelect(elLi){
-    elLi.classList.toggle('select')
-    var elSelects=document.querySelectorAll('.select');
+    elLi.classList.toggle('active')
+    var elSelects=document.querySelectorAll('.active');
     for (var i=0;i<elSelects.length;i++){
         if(elSelects[i]===elLi){
             continue;
         }
-        elSelects[i].classList.remove('select');
+        elSelects[i].classList.remove('active');
     }
 }
